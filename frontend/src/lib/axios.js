530a1dd8ec392d5cbf9e301e038f7ff1ec2d11b0
Shivamjan24 <shivamkaushikjan@gmail.com>
2025-03-14 14:57:41 +0530

    last time

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance=axios.create({
-    baseURL: import.meta.env.MODE==="development" ? "http://localhost:5001/api" : "https://real-time-chat-application-beta.vercel.app/api",
+    baseURL: import.meta.env.MODE==="development" ? "http://localhost:5001/api" : "api",
     withCredentials:true
 })
 
